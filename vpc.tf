b0bbdf5f6ae23476175e31cdd4fdb11c3ce0bde8
shaazAhmed91 <shaaz.ahmed573@gmail.com>
2026-05-18 20:11:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -47,7 +47,7 @@ resource "aws_internet_gateway" "main" {
 resource "aws_instance" "web" {
   ami                = "ami-07a00cf47dbbc844c" # Amazon Linux 2 (example)
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public.id
+  subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   associate_public_ip_address = true
