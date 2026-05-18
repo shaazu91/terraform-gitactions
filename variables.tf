299f2f2f35ea5c4a95ab3110c8e640adeb2708f8
shaazAhmed91 <shaaz.ahmed573@gmail.com>
2026-05-18 19:34:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,11 +22,7 @@ variable "public_subnet_cidrs" {
   default     = ["10.0.1.0/24"]
 }
 
-#variable "private_subnet_cidrs" {
- # description = "List of CIDR blocks for private subnets"
- # type        = list(string)
- # default     = ["10.0.11.0/24"]
-#}
+
 
 variable "public_subnet_count" {
   description = "Number of public subnets to create"
@@ -34,11 +30,6 @@ variable "public_subnet_count" {
   default     = 1
 }
 
-variable "private_subnet_count" {
-  description = "Number of private subnets to create"
-  type        = number
-  default     = 1
-}
 
 variable "availability_zones" {
   description = "List of availability zones"
@@ -49,7 +40,7 @@ variable "availability_zones" {
 variable "ami_id" {
   description = "AMI ID for EC2 instances"
   type        = string
-  default     = "ami-0c398cb65a93047f2" # Ubuntu 22.04 LTS
+  default     = "ami-07a00cf47dbbc844c" # Ubuntu 22.04 LTS
 }
 
 variable "instance_type" {
